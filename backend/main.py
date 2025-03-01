08b61987c65b19d30548b59b21b6842642882d57
Adam Elsayed <adamelsayyed06@gmail.com>
2025-03-01 02:30:26 -0500

    rm unnecessary api endpoint

--- backend/main.py
+++ backend/main.py
@@ -5,10 +5,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/get-response', methods = ['POST']) #get response from openai, but passes in user input as a query
-def get_response():
-    return [] #api call to get_openai as well as user input 
-
 
 @app.route('/history', methods =['GET'])
 def get_history():
