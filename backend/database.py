2ba228edacfed0a963cefa7f75d1871b1e709823
Adam Elsayed <adamelsayyed06@gmail.com>
2025-03-06 11:09:40 -0500

    quick bug fix

--- backend/database.py
+++ backend/database.py
@@ -13,7 +13,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(120), unique = True, nullable = False)
     name = db.Column(db.String(40), unique = False, nullable = False)
-    authProvider = db.Column()
+    authProvider = db.Column(db.String(40), unique = False, nullable = False)
 
 class Meeting(db.Model):
     id = db.Column(db.Integer, primary_key = True)
