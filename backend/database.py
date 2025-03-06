4a74012a2fc2ae216d29b0dd0a75583ec94510ed
Adam Elsayed <adamelsayyed06@gmail.com>
2025-03-05 22:43:40 -0500

    moved database from db -> config and added testingConfig

--- backend/database.py
+++ backend/database.py
@@ -1,2 +1,33 @@
 #configure SQL, define database model
         
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def init_db(app):
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    name = db.Column(db.String(40), unique = False, nullable = False)
+    authProvider = db.Column()
+
+class Meeting(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    zoom_meeting_id = db.Column(db.String(50), nullable = False, unique = True)
+    topic = db.Column(db.String(255))
+
+class Transcript(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    meeting_id = db.Column(db.Integer, db.ForeignKey('meeting.id'), nullable = False) #every transcript paired to a meeting
+    transcript_text = db.Column(db.Text(500))
+
+    #potentially add relationship between Transcript/Meeting w/back_populates so u can pull transcript of specific meeting, or meeting of specific transcript easily
+
+class Summary(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    meeting_id = db.Column(db.Integer, db.ForeignKey('meeting.id'), nullable = False) #every meeting needs a summary
+    summary_text = db.Column(db.Text(500))
--- config.py
+++ config.py
@@ -1,33 +1,8 @@
 #database settings, JWT secrets, credentials etc.
 
-from flask_sqlalchemy import SQLAlchemy
+import os
 
-db = SQLAlchemy()
+class TestingConfig():
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///test.db'
+    TESTING = True  #Enable testing mode (disables error catching)
 
-def init_db(app):
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(120), unique = True, nullable = False)
-    name = db.Column(db.String(40), unique = False, nullable = False)
-    authProvider = db.Column()
-
-class Meeting(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    zoom_meeting_id = db.Column(db.String(50), nullable = False, unique = True)
-    topic = db.Column(db.String(255))
-
-class Transcript(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    meeting_id = db.Column(db.Integer, db.ForeignKey('meeting.id'), nullable = False) #every transcript paired to a meeting
-    transcript_text = db.Column(db.Text(500))
-
-    #potentially add relationship between Transcript/Meeting w/back_populates so u can pull transcript of specific meeting, or meeting of specific transcript easily
-
-class Summary(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    meeting_id = db.Column(db.Integer, db.ForeignKey('meeting.id'), nullable = False) #every meeting needs a summary
-    summary_text = db.Column(db.Text(500))
