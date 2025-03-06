03ebeafbd0461131142b0251e6401328f6af5d19
Adam Elsayed <adamelsayyed06@gmail.com>
2025-03-05 22:24:22 -0500

    capitalization fix + meeting db

--- config.py
+++ config.py
@@ -9,9 +9,15 @@ def init_db(app):
     with app.app_context():
         db.create_all()
 
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(120), unique = True, nullable = False)
     name = db.Column(db.String(40), unique = False, nullable = False)
     authProvider = db.Column()
 
+class Meeting(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    zoom_meeting_id = db.Column(db.String(50), nullable = False, unique = True)
+    topic = db.Column(db.String(255))
+
+class Transcript(db.Model)
